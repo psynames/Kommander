5ea152e37ba1b4979cfe5f333d2e6cb45ce974dc
psyname <48337480+psynames@users.noreply.github.com>
2020-09-04 21:23:17 +0200

    created Commandscontroller and repositories

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kommander.Data;
+using Kommander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kommander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase //without views support 
+    {
+        private readonly MockCommanderRepo _repo = new MockCommanderRepo();
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commands = _repo.GetAppCommands();
+            return Ok(commands);
+        }
+
+        //GET api/commands/{int}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var command = _repo.GetCommandById(id);
+            return Ok(command);
+        }
+    }
+}
